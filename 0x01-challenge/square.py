3dc79631c36417e1d1ade1781375771f7e884a26
Charaf-Z <charafomarzitouni@gmail.com>
2024-02-13 21:53:50 +0100

    updat squar

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
-"""Define a Square class with width and height attributes."""
+""" Module for square class"""
 
 
-class Square:
-    """Represent a square with width and height attributes."""
-
+class Square():
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialize a Square object.
-
-        Args:
-            *args: Variable length argument list (unused).
-            **kwargs: Arbitrary keyword arguments to set attributes.
-
-        Attributes:
-            width (int): The width of the square.
-            height (int): The height of the square.
-        """
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Area of the square"""
-        return self.width * self.width
+        """ Area of the square """
+        return self.width * self.height
 
     def permiter_of_my_square(self):
-        """Calculate the perimeter of the square."""
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Represent the square as a string."""
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """Create a Square object, print its details, area, and perimeter."""
+    """ Create a square object """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
